d59d2ec4bc705128a7609299e70380ad60dd2406
agentdimko <dimanew2004@mail.ru>
2019-10-19 20:22:09 +0300

    Refactoring all classes

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,9 +1,12 @@
+import com.dimko.webapp.model.Resume;
+import com.dimko.webapp.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for com.dimko.webapp.storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -11,7 +14,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
+        Resume resume;
         while (true) {
             System.out.print("Введите одну из команд - (list | save uuid | delete uuid | update uuid | get uuid | " +
                     "clear | exit): ");
@@ -32,9 +35,8 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.uuid = uuid;
-                    ARRAY_STORAGE.save(r);
+                    resume = new Resume(uuid);
+                    ARRAY_STORAGE.save(resume);
                     printAll();
                     break;
                 case "delete":
@@ -42,8 +44,7 @@ public class MainArray {
                     printAll();
                     break;
                 case "update":
-                    Resume resume = new Resume();
-                    resume.uuid = uuid;
+                    resume = new Resume(uuid);
                     ARRAY_STORAGE.update(resume);
                     printAll();
                     break;
---
+++ src/com/dimko/webapp/storage/ArrayStorage.java
@@ -0,0 +1,74 @@
+package com.dimko.webapp.storage;
+
+import com.dimko.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private final int STORAGE_FULL_SIZE = 10_000;
+    private int size;
+    private Resume[] storage = new Resume[STORAGE_FULL_SIZE];
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume resume) {
+        int index = findResumeIndex(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+        }
+    }
+
+    public void save(Resume r) {
+        if (size == STORAGE_FULL_SIZE) {
+            System.out.println("Storage is full!!");
+            return;
+        }
+        if (findResumeIndex(r.getUuid()) != -1) {
+            System.out.println("com.dimko.webapp.model.Resume wit uuid = " + r.getUuid() + " is already exist");
+            return;
+        }
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid) {
+        int index = findResumeIndex(uuid);
+        return (index != -1) ? storage[index] : null;
+    }
+
+    public void delete(String uuid) {
+        int index = findResumeIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int findResumeIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        System.out.println("com.dimko.webapp.model.Resume with uuid = " + uuid + " not found!");
+        return -1;
+    }
+}
