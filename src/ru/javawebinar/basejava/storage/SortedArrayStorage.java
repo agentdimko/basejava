70fa1b3818fa400c2daaac275c1060cfff762563
agentdimko <dimanew2004@mail.ru>
2019-10-21 19:16:52 +0300

    Implementation of save() method in the class SortedArrayStorage is changed

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -22,41 +22,25 @@ public class SortedArrayStorage extends AbstractArrayStorage {
             System.out.println("Unabel to save. Storage is full!!");
             return;
         }
-        if (getIndex(resume.getUuid()) >= 0) {
+        int index = getIndex(resume.getUuid());
+        if(index > 0) {
             System.out.println("Unable to save. Resume with uuid " + resume.getUuid() + " is already exist");
             return;
         }
-        storage[size] = resume;
+        index = - (index + 1);
+        if (index == size){
+            storage[size] = resume;
+            size++;
+            return;
+        }
+        System.arraycopy(storage, index, storage, index +1, size - index);
+        storage[index] = resume;
         size++;
-        sortStorage();
     }
 
 
     @Override
     protected int getIndex(String uuid) {
         return Arrays.binarySearch(storage, 0, size, new Resume(uuid));
-    }
-
-    private void sortStorage() {
-        for (int i = 0; i < size; i++) {
-            int j = findLowestIndex(i);
-            swapElements(i, j);
-        }
-    }
-
-    private void swapElements(int i, int j) {
-        Resume tmp = storage[i];
-        storage[i] = storage[j];
-        storage[j] = tmp;
-    }
-
-    private int findLowestIndex(int start) {
-        int lowIndex = start;
-        for (int i = 0; i < size; i++) {
-            if ((storage[i].compareTo(storage[lowIndex])) == -1) {
-                lowIndex = i;
-            }
-        }
-        return lowIndex;
     }
 }
