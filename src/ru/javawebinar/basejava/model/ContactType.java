722d816df1b16de7b66f1993ee6be81b23813519
agentdimko <dimanew2004@mail.ru>
2019-11-03 19:57:46 +0300

    HW7 created ContactType and SectionType

---
+++ src/ru/javawebinar/basejava/model/ContactType.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava.model;
+
+public enum ContactType {
+    PHONE("Тел.:"),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    LINKEDINPROFILE("Профиль LinkedId"),
+    GITHUBPROFILE("Профиль GitHub"),
+    STACKOVERFLOWPROFILE("Профиль Stackoverflow"),
+    HOMEPAGE("Домашняя страница");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
