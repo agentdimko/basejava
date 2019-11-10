f1aa91b1444ae82ad2bea018fa08b7063ae0d2b1
agentdimko <dimanew2004@mail.ru>
2019-11-10 15:46:49 +0300

    HW8 Recursive traversal and file name output in project directories and subdirectories(method printFileNames)

---
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+        File dir = new File("./src/ru/javawebinar/basejava");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : dir.list()) {
+                System.out.println(name);
+            }
+        }
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        printFileNames(new File("."));
+    }
+
+    public static void printFileNames(File directory) {
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            for (File file : files) {
+                if (!file.isDirectory()) {
+                    System.out.println(file.toString());
+                } else {
+                    printFileNames(file);
+                }
+            }
+        }
+    }
+}
