906d70aa36769a6be9ed2a312b2b483b3245bea8
agentdimko <dimanew2004@mail.ru>
2019-12-15 21:24:23 +0300

    Lesson 14

--- src/ru/javawebinar/basejava/storage/SQLStorage.java
+++ src/ru/javawebinar/basejava/storage/SQLStorage.java
@@ -1,13 +1,16 @@
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.ContactType;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.sql.SqlHelper;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SQLStorage implements Storage {
     private SqlHelper sqlHelper;
@@ -16,6 +19,7 @@ public class SQLStorage implements Storage {
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
+    //готово
     @Override
     public void clear() {
         sqlHelper.execute("DELETE FROM resume");
@@ -33,28 +37,55 @@ public class SQLStorage implements Storage {
         });
     }
 
+    //готово
     @Override
     public void save(Resume resume) {
-        sqlHelper.<Void>execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", (ps) -> {
-            ps.setString(1, resume.getUuid());
-            ps.setString(2, resume.getFullName());
-            ps.execute();
-            return null;
-        });
+        sqlHelper.transactionalExecute(conn -> {
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+                        ps.setString(1, resume.getUuid());
+                        ps.setString(2, resume.getFullName());
+                        ps.execute();
+                    }
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO contact (resume_uuid, type, value) VALUES " +
+                            "(?, ?,?)")) {
+                        for (Map.Entry<ContactType, String> e : resume.getContacts().entrySet()) {
+                            ps.setString(1, resume.getUuid());
+                            ps.setString(2, e.getKey().name());
+
+                            ps.setString(3, e.getValue());
+                            ps.addBatch();
+                        }
+                        ps.executeBatch();
+                    }
+                    return null;
+                }
+        );
     }
 
+    //Готово
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid = ?", (ps) -> {
+        return sqlHelper.execute("SELECT r.uuid, r.full_name, c.type, c.value FROM resume r\n" +
+                "LEFT JOIN contact c\n" +
+                "ON r.uuid = c.resume_uuid\n" +
+                "WHERE r.uuid = ?", (ps) -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
-            return new Resume(uuid, rs.getString("full_name"));
+            Resume resume = new Resume(uuid, rs.getString("full_name"));
+            do {
+                String value = rs.getString("value");
+                ContactType type = ContactType.valueOf(rs.getString("type"));
+                resume.addContact(type, value);
+            } while (rs.next());
+            return resume;
         });
     }
 
+
+    //Готово
     @Override
     public void delete(String uuid) {
         sqlHelper.execute("DELETE FROM resume WHERE uuid = ?", (ps) -> {
@@ -68,6 +99,7 @@ public class SQLStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+
         return sqlHelper.execute("SELECT * FROM resume ORDER BY full_name, uuid", (ps) -> {
             List<Resume> list = new ArrayList<>();
             ResultSet rs = ps.executeQuery();
@@ -80,6 +112,8 @@ public class SQLStorage implements Storage {
         });
     }
 
+
+    //Готово
     @Override
     public int size() {
         return sqlHelper.execute("SELECT COUNT(*) AS count FROM resume", (ps) -> {
